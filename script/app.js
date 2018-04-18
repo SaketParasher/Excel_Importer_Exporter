486eff2c6786050cd3ce64decce7a8f87512a6b2
SaketParasher <saket.parasher@gmail.com>
2018-04-18 19:22:11 +0530

    second cmt frm ofc

--- script/app.js
+++ script/app.js
@@ -21,6 +21,7 @@ module.exports.generateFakeData = function(req,res){
     fakeData.name = faker.name.findName();
     fakeData.email = faker.internet.email();
     fakeData.contact = faker.phone.phoneNumber();
+    //console.log('phone :- '+faker.phone.phoneNumberFormat());
     fakeData.gender = 'Others';
     fakeData.message = faker.lorem.lines();
     fakeData.mailOffers = faker.random.boolean();
@@ -33,7 +34,50 @@ module.exports.generateFakeData = function(req,res){
 };
 
 module.exports.saveToExcel  = function(req,res){
-    var data = req.body;
-    //console.log(data);
-    res.send();
+var data = req.body;
+    for(var prop in data){
+        console.log(typeof data[prop] +'  '+data[prop]);
+    }
+var xl = require('excel4node');
+ 
+// Create a new instance of a Workbook class
+var wb = new xl.Workbook();
+ 
+// Add Worksheets to the workbook
+var ws = wb.addWorksheet('Sheet 1');
+//var ws2 = wb.addWorksheet('Sheet 2');
+ 
+// Create a reusable style
+var style = wb.createStyle({
+    font: {
+        color: '#000000',
+        size: 12
+    },
+    numberFormat: '$#,##0.00; ($#,##0.00); -'
+});
+ 
+// Set value of cell A1 to 100 as a number type styled with paramaters of style
+    ws.cell(1,1).string('Reservation ID').style(style);
+    ws.cell(1,2).string('Name').style(style);
+    ws.cell(1,3).string('Email').style(style);
+    ws.cell(1,4).string('Contact').style(style);
+    ws.cell(1,5).string('Gender').style(style);
+    ws.cell(1,6).string('Message').style(style);
+    ws.cell(1,7).string('Send Mails').style(style);
+    ws.cell(1,8).string('Prime Member').style(style);
+ 
+    ws.cell(2,1).string(data.rid).style(style);
+    ws.cell(2,2).string(data.name).style(style);
+    ws.cell(2,3).string(data.email).style(style);
+    ws.cell(2,4).string(data.contact).style(style);
+    ws.cell(2,5).string(data.gender).style(style);
+    ws.cell(2,6).string(data.message).style(style);
+    ws.cell(2,7).bool(Boolean(data.mailOffers)).style(style);
+    ws.cell(2,8).bool(Boolean(data.primeMember)).style(style);
+    
+    console.log(xl.getExcelAlpha(10));
+ 
+    
+wb.write('Excel.xlsx');
+res.send();
 };
