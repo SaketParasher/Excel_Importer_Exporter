ed33441fe4485256d6c0521b6beb245bbf745bcc
SaketParasher <saket.parasher@gmail.com>
2018-04-19 18:25:58 +0530

    commt frm ofc

--- script/app.js
+++ script/app.js
@@ -1,6 +1,11 @@
-
+var fs = require('fs');
 var faker = require('faker');
 var excel = require('excel4node');
+var excelRead = require('excel').default;
+var xlsx = require('node-xlsx');
+var xl = require('excel4node');
+
+var Excel = require('exceljs');
 
 module.exports.generateFakeData = function(req,res){
     
@@ -35,49 +40,71 @@ module.exports.generateFakeData = function(req,res){
 
 module.exports.saveToExcel  = function(req,res){
 var data = req.body;
-    for(var prop in data){
-        console.log(typeof data[prop] +'  '+data[prop]);
+    
+
+      
+     // var sheet = workbook.addWorksheet('My Sheet');   
+ 
+/*if (!fs.existsSync(__dirname+'/exex.xlsx')) {
+      console.log('true');
+      var sheet = workbook.addWorksheet('My Sheet');   
+  }*/
+//workbook.eachSheet(function(worksheet, sheetId) {
+    
+//});
+    
+    if(!global['workbook']){
+        console.log('In Workbook');
+      var workbook = new Excel.Workbook();  
+        global['workbook']=workbook;
+    }else{
+        workbook = global['workbook'];
     }
-var xl = require('excel4node');
- 
-// Create a new instance of a Workbook class
-var wb = new xl.Workbook();
- 
-// Add Worksheets to the workbook
-var ws = wb.addWorksheet('Sheet 1');
-//var ws2 = wb.addWorksheet('Sheet 2');
- 
-// Create a reusable style
-var style = wb.createStyle({
-    font: {
-        color: '#000000',
-        size: 12
-    },
-    numberFormat: '$#,##0.00; ($#,##0.00); -'
-});
- 
-// Set value of cell A1 to 100 as a number type styled with paramaters of style
-    ws.cell(1,1).string('Reservation ID').style(style);
-    ws.cell(1,2).string('Name').style(style);
-    ws.cell(1,3).string('Email').style(style);
-    ws.cell(1,4).string('Contact').style(style);
-    ws.cell(1,5).string('Gender').style(style);
-    ws.cell(1,6).string('Message').style(style);
-    ws.cell(1,7).string('Send Mails').style(style);
-    ws.cell(1,8).string('Prime Member').style(style);
- 
-    ws.cell(2,1).string(data.rid).style(style);
-    ws.cell(2,2).string(data.name).style(style);
-    ws.cell(2,3).string(data.email).style(style);
-    ws.cell(2,4).string(data.contact).style(style);
-    ws.cell(2,5).string(data.gender).style(style);
-    ws.cell(2,6).string(data.message).style(style);
-    ws.cell(2,7).bool(Boolean(data.mailOffers)).style(style);
-    ws.cell(2,8).bool(Boolean(data.primeMember)).style(style);
     
-    console.log(xl.getExcelAlpha(10));
- 
+    if(!global['sheet']){
+      var sheet = workbook.addWorksheet('My Sheet');
+        global['sheet'] = sheet;
+    }else{
+        sheet = global['sheet'];
+    }
     
-wb.write('Excel.xlsx');
+    if(!global['worksheet']){
+      var worksheet = workbook.getWorksheet('My Sheet');
+       worksheet.columns = [
+            { header: 'Id', key: 'id', width: 10 },
+            { header: 'Name', key: 'name', width: 32 },
+            { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
+        ];
+        global['worksheet'] = worksheet;
+    }else{
+        worksheet = global['worksheet'];
+    }
+    
+//var worksheet = workbook.getWorksheet('My Sheet');
+    /*
+    worksheet.columns = [
+    { header: 'Id', key: 'id', width: 10 },
+    { header: 'Name', key: 'name', width: 32 },
+    { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
+];
+    */
+
+  /*  
+worksheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
+worksheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
+worksheet.addRow([3, 'Sam', new Date()]);
+worksheet.addRow({id: 4, name: 'Don Jon', dob: new Date()});
+    worksheet.addRow({id: 5, name: 'Don Jon', dob: new Date()});
+*/    
+    worksheet.addRow({id: data.rid, name: data.name, DOB: data.gender});
+    
+    workbook.xlsx.writeFile(__dirname+'/exex.xlsx')
+    .then(function() {
+        console.log('Data Added to Sheet');
+    });
+
+    
+
 res.send();
 };
+
